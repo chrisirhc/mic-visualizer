0e0a89d3f034bfa3a670031f3149b5fa9f4b59e9
Chris Chua <chris.sirhc@gmail.com>
2024-04-06 21:26:35 +0800

    accessed the microphone
    Very useful tutorial on https://dobrian.github.io/cmp/topics/sample-recording-and-playback-with-web-audio-api/3.microphone-input-and-recording.html

---
+++ index.js
@@ -0,0 +1,17 @@
+// Maybe use mjs?
+
+const audioCtx = new AudioContext();
+if (navigator.mediaDevices) {
+  navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
+    const microphone = audioCtx.createMediaStreamSource(stream);
+    // `microphone` can now act like any other AudioNode
+  }).catch((err) => {
+    // browser unable to access microphone
+    // (check to see if microphone is attached)
+    console.error('error accessing microphone', err);
+  });
+} else {
+  // browser unable to access media devices
+  // (update your browser)
+  console.error('unable to access media devices')
+}
