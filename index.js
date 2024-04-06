cfad10d128b4a67b9cabddc4236200974c41243a
Chris Chua <chris.sirhc@gmail.com>
2024-04-06 21:41:31 +0800

    added demo oscilloscope but not working
    Not sure why but doesn't show any activity with the microphone.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // Maybe use mjs?
+const canvas = document.getElementById("oscilloscope");
+const canvasCtx = canvas.getContext("2d");
 
 const audioCtx = new AudioContext();
+const analyser = audioCtx.createAnalyser();
+analyser.fftSize = 2048;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+
 if (navigator.mediaDevices) {
   navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
     const microphone = audioCtx.createMediaStreamSource(stream);
     // `microphone` can now act like any other AudioNode
+
+    microphone.connect(analyser);
+    analyser.getByteTimeDomainData(dataArray);
+
+    draw();
   }).catch((err) => {
     // browser unable to access microphone
     // (check to see if microphone is attached)
@@ -14,4 +26,37 @@ if (navigator.mediaDevices) {
   // browser unable to access media devices
   // (update your browser)
   console.error('unable to access media devices')
+}
+
+function draw() {
+  requestAnimationFrame(draw);
+
+  analyser.getByteTimeDomainData(dataArray);
+
+  canvasCtx.fillStyle = "rgb(200 200 200)";
+  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+  canvasCtx.lineWidth = 2;
+  canvasCtx.strokeStyle = "rgb(0 0 0)";
+
+  canvasCtx.beginPath();
+
+  const sliceWidth = (canvas.width * 1.0) / bufferLength;
+  let x = 0;
+
+  for (let i = 0; i < bufferLength; i++) {
+    const v = dataArray[i] / 128.0;
+    const y = (v * canvas.height) / 2;
+
+    if (i === 0) {
+      canvasCtx.moveTo(x, y);
+    } else {
+      canvasCtx.lineTo(x, y);
+    }
+
+    x += sliceWidth;
+  }
+
+  canvasCtx.lineTo(canvas.width, canvas.height / 2);
+  canvasCtx.stroke();
 }
