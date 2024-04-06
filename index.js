d3afcadbea9a3b14a7e9481124457e490ccdaced
Chris Chua <chris.sirhc@gmail.com>
2024-04-06 22:31:40 +0800

    something on the screen
    audio context has to be started with an on-page gesture, like a button click.

--- index.js
+++ index.js
@@ -2,61 +2,62 @@
 const canvas = document.getElementById("oscilloscope");
 const canvasCtx = canvas.getContext("2d");
 
-const audioCtx = new AudioContext();
-const analyser = audioCtx.createAnalyser();
-analyser.fftSize = 2048;
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
+function start() {
+  const audioCtx = new AudioContext();
+  const analyser = audioCtx.createAnalyser();
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
 
-if (navigator.mediaDevices) {
-  navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
-    const microphone = audioCtx.createMediaStreamSource(stream);
-    // `microphone` can now act like any other AudioNode
+  if (navigator.mediaDevices) {
+    navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
+      const microphone = audioCtx.createMediaStreamSource(stream);
+      // `microphone` can now act like any other AudioNode
 
-    microphone.connect(analyser);
-    analyser.getByteTimeDomainData(dataArray);
+      microphone.connect(analyser);
+      analyser.getByteFrequencyData(dataArray);
 
-    draw();
-  }).catch((err) => {
-    // browser unable to access microphone
-    // (check to see if microphone is attached)
-    console.error('error accessing microphone', err);
-  });
-} else {
-  // browser unable to access media devices
-  // (update your browser)
-  console.error('unable to access media devices')
-}
-
-function draw() {
-  requestAnimationFrame(draw);
-
-  analyser.getByteTimeDomainData(dataArray);
-
-  canvasCtx.fillStyle = "rgb(200 200 200)";
-  canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-  canvasCtx.lineWidth = 2;
-  canvasCtx.strokeStyle = "rgb(0 0 0)";
-
-  canvasCtx.beginPath();
-
-  const sliceWidth = (canvas.width * 1.0) / bufferLength;
-  let x = 0;
-
-  for (let i = 0; i < bufferLength; i++) {
-    const v = dataArray[i] / 128.0;
-    const y = (v * canvas.height) / 2;
-
-    if (i === 0) {
-      canvasCtx.moveTo(x, y);
-    } else {
-      canvasCtx.lineTo(x, y);
-    }
-
-    x += sliceWidth;
+      draw();
+    }).catch((err) => {
+      // browser unable to access microphone
+      // (check to see if microphone is attached)
+      console.error('error accessing microphone', err);
+    });
+  } else {
+    // browser unable to access media devices
+    // (update your browser)
+    console.error('unable to access media devices')
   }
 
-  canvasCtx.lineTo(canvas.width, canvas.height / 2);
-  canvasCtx.stroke();
+  function draw() {
+    requestAnimationFrame(draw);
+
+    analyser.getByteFrequencyData(dataArray);
+
+    canvasCtx.fillStyle = "rgb(200 200 200)";
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = "rgb(0 0 0)";
+
+    canvasCtx.beginPath();
+
+    const sliceWidth = (canvas.width * 1.0) / bufferLength;
+    let x = 0;
+
+    for (let i = 0; i < bufferLength; i++) {
+      const v = dataArray[i] / 128.0;
+      const y = (v * canvas.height) / 2;
+
+      if (i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height / 2);
+    canvasCtx.stroke();
+  }
 }
